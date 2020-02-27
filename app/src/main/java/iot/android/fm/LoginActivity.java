a15b38ccbb5fdd8ab7c229be9c1ff515536a103f
Laras <pertiwi.laras13@gmail.com>
2020-02-27 19:35:23 +0700

    finished #1_Login_UI

--- app/src/main/java/iot/android/fm/LoginActivity.java
+++ app/src/main/java/iot/android/fm/LoginActivity.java
@@ -1,4 +1,10 @@
 package iot.android.fm;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
 public class LoginActivity {
 }
