4668f0e135c7a1b7c8418e9d3aee41a31f71054e
Laras <pertiwi.laras13@gmail.com>
2020-02-28 21:30:18 +0700

    Setting Login

--- app/src/main/java/iot/android/fm/LoginActivity.java
+++ app/src/main/java/iot/android/fm/LoginActivity.java
@@ -2,9 +2,39 @@ package iot.android.fm;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import iot.android.fm.databinding.ActivityLoginBinding;
 
-public class LoginActivity {
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ActivityLoginBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+
+        binding.buttonLogin.setOnClickListener(this);
+        binding.forgotPass.setOnClickListener(this);
+        binding.createAcc.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonLogin:
+                Toast.makeText(this, "login", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.forgotPass:
+                Toast.makeText(this, "forgot", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.createAcc:
+                Toast.makeText(this, "create new", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
 }
