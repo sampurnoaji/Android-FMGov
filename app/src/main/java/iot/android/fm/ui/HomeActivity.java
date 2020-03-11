536d408c321589e46b44719e278ba032239b48c1
Laras <pertiwi.laras13@gmail.com>
2020-03-12 00:40:20 +0700

    Home template (just open camera, not success scan qr code)

--- app/src/main/java/iot/android/fm/ui/HomeActivity.java
+++ app/src/main/java/iot/android/fm/ui/HomeActivity.java
@@ -3,13 +3,36 @@ package iot.android.fm.ui;
 import androidx.appcompat.app.AppCompatActivity;
 import iot.android.fm.R;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener{
+
+    ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //set click event of image button but not working
+        imageButton = (ImageButton)findViewById(R.id.imageButton);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, CameraScanner.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
