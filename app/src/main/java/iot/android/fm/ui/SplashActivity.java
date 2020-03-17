d2e4abe3f63f91e926f7e7904ac31f68413f360d
Laras <pertiwi.laras13@gmail.com>
2020-03-17 19:18:32 +0700

    Splash screen

---
+++ app/src/main/java/iot/android/fm/ui/SplashActivity.java
@@ -0,0 +1,28 @@
+package iot.android.fm.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+import iot.android.fm.R;
+import retrofit2.http.Header;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run(){
+                Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
