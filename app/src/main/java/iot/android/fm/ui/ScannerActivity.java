5e4510bf27c9150e754d32cbc0f556111e4d5ab7
Sampurno Aji <man.sanji23@gmail.com>
2020-03-30 18:57:45 +0700

    cleaning code

--- app/src/main/java/iot/android/fm/ui/ScannerActivity.java
+++ app/src/main/java/iot/android/fm/ui/ScannerActivity.java
@@ -1,18 +1,17 @@
 package iot.android.fm.ui;
 
 import android.Manifest;
-import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.media.AudioManager;
 import android.media.ToneGenerator;
 import android.os.Bundle;
-import android.os.Vibrator;
 import android.util.Log;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.databinding.DataBindingUtil;
 
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
@@ -21,50 +20,46 @@ import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 import java.io.IOException;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 import iot.android.fm.R;
+import iot.android.fm.databinding.ActivityScannerBinding;
 
 public class ScannerActivity extends AppCompatActivity {
 
-    SurfaceView surfaceQRcode;
+    private ActivityScannerBinding binding;
     BarcodeDetector barcodeDetector;
     CameraSource cameraSource;
-    TextView txtResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scanner);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_scanner);
         setupScanQRCode();
     }
 
     private void setupScanQRCode() {
-        surfaceQRcode = (SurfaceView)findViewById(R.id.surfaceView);
-        txtResult = findViewById(R.id.txtResult);
         barcodeDetector = new BarcodeDetector.Builder(this)
                 .setBarcodeFormats(Barcode.ALL_FORMATS)
                 .build();
         cameraSource = new CameraSource
                 .Builder(getApplicationContext(), barcodeDetector)
-                .setRequestedPreviewSize(1024,768)
+                .setRequestedPreviewSize(1024, 768)
                 .setAutoFocusEnabled(true)
                 .build();
 
         //Add Event
-        surfaceQRcode.getHolder().addCallback(new SurfaceHolder.Callback() {
+        binding.surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(SurfaceHolder surfaceHolder) {
                 try {
                     if (ActivityCompat.checkSelfPermission(getApplicationContext(),
                             Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
-                        cameraSource.start(surfaceQRcode.getHolder());
+                        cameraSource.start(binding.surfaceView.getHolder());
                     } else {
                         ActivityCompat.requestPermissions(ScannerActivity.this,
                                 new String[]{Manifest.permission.CAMERA}, 1024);
                     }
                 } catch (IOException e) {
-                    Log.e("Camera start error--> ", e.getMessage().toString());
+                    Log.e("Camera start error--> ", e.getMessage());
                 }
             }
 
@@ -87,31 +82,21 @@ public class ScannerActivity extends AppCompatActivity {
 
             @Override
             public void receiveDetections(Detector.Detections<Barcode> detections) {
-               SparseArray<Barcode> qrcodes = detections.getDetectedItems();
-                if(qrcodes.size() > 0) {
-//                    barcodeDetector.release();
-//                    ToneGenerator toneGenerator = new ToneGenerator(AudioManager.STREAM_NOTIFICATION, 1000);
-//                    toneGenerator.startTone(ToneGenerator.TONE_PROP_BEEP,150);
-//                    String scanResult = qrcodes.valueAt(0).displayValue;
+                SparseArray<Barcode> qrcodes = detections.getDetectedItems();
+                if (qrcodes.size() > 0) {
+                    barcodeDetector.release();
+                    ToneGenerator toneGenerator = new ToneGenerator(AudioManager.STREAM_NOTIFICATION, 1000);
+                    toneGenerator.startTone(ToneGenerator.TONE_PROP_BEEP, 150);
+                    String scanResult = qrcodes.valueAt(0).displayValue;
 
-                    txtResult.post(new Runnable() {
-                      @Override
-                      public void run() {
-                          //Create vibrate
-                          Vibrator vibrator = (Vibrator)getApplication().getSystemService(Context.VIBRATOR_SERVICE);
-                          vibrator.vibrate(1000);
-                          txtResult.setText(qrcodes.valueAt(0).displayValue);
-                      }
-                    });
-
-//                    Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
-//                    startActivity(intent);
+                    binding.txtResult.post(() -> binding.txtResult.setText(scanResult));
                 }
             }
         });
     }
+
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
         setupScanQRCode();
     }
