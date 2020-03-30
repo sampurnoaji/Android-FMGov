2e0ce1e2f7636537f3fb10c15b381b669ce8134b
Laras <pertiwi.laras13@gmail.com>
2020-03-30 12:00:46 +0700

    Implement QR Code scanner

--- app/src/main/java/iot/android/fm/ui/ScannerActivity.java
+++ app/src/main/java/iot/android/fm/ui/ScannerActivity.java
@@ -1,10 +1,18 @@
 package iot.android.fm.ui;
 
 import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.util.Log;
+import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.widget.TextView;
 
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
@@ -13,66 +21,50 @@ import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 import java.io.IOException;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import iot.android.fm.R;
 
 public class ScannerActivity extends AppCompatActivity {
 
-    SurfaceView surfaceView;
+    SurfaceView surfaceQRcode;
     BarcodeDetector barcodeDetector;
     CameraSource cameraSource;
-    final int RequestCameraPermissionID = 1001;
+    TextView txtResult;
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-       switch (requestCode)
-       {
-           case RequestCameraPermissionID:
-           {
-               if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
-               { if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
-                   return;
-                   try {
-                       cameraSource.start(surfaceView.getHolder());
-                   } catch (IOException e) {
-                       e.printStackTrace();
-                   }
-               }
-           }
-           break;
-       }
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanner);
+        setupScanQRCode();
+    }
 
-        surfaceView = (SurfaceView)findViewById(R.id.surfaceView);
-
+    private void setupScanQRCode() {
+        surfaceQRcode = (SurfaceView)findViewById(R.id.surfaceView);
+        txtResult = findViewById(R.id.txtResult);
         barcodeDetector = new BarcodeDetector.Builder(this)
-                .setBarcodeFormats(Barcode.QR_CODE)
+                .setBarcodeFormats(Barcode.ALL_FORMATS)
                 .build();
         cameraSource = new CameraSource
-                .Builder(this, barcodeDetector)
-                .setRequestedPreviewSize(640,480)
+                .Builder(getApplicationContext(), barcodeDetector)
+                .setRequestedPreviewSize(1024,768)
+                .setAutoFocusEnabled(true)
                 .build();
 
         //Add Event
-        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+        surfaceQRcode.getHolder().addCallback(new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(SurfaceHolder surfaceHolder) {
-                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
-                    //Request permission
-                    ActivityCompat.requestPermissions(ScannerActivity.this,
-                            new String[]{Manifest.permission.CAMERA}, RequestCameraPermissionID);
-
                 try {
-                    cameraSource.start(surfaceView.getHolder());
+                    if (ActivityCompat.checkSelfPermission(getApplicationContext(),
+                            Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                        cameraSource.start(surfaceQRcode.getHolder());
+                    } else {
+                        ActivityCompat.requestPermissions(ScannerActivity.this,
+                                new String[]{Manifest.permission.CAMERA}, 1024);
+                    }
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    Log.e("Camera start error--> ", e.getMessage().toString());
                 }
             }
 
@@ -95,20 +87,32 @@ public class ScannerActivity extends AppCompatActivity {
 
             @Override
             public void receiveDetections(Detector.Detections<Barcode> detections) {
-               //final SparseArray<Barcode> qrcodes = detections.getDetectedItems();
-                //if(qrcodes.size() != 0)
-                //{
-                   // txtResult.post(new Runnable() {
-                      //  @Override
-                      //  public void run() {
-                            //Create vibrate
-                            //Vibrator vibrator = (Vibrator)getApplication().getSystemService(Context.VIBRATOR_SERVICE);
-                           // vibrator.vibrate(1000);
-                           // txtResult.setText(qrcodes.valueAt(0).displayValue);
-                       // }
-                  //  });
-               // }
+               SparseArray<Barcode> qrcodes = detections.getDetectedItems();
+                if(qrcodes.size() > 0) {
+//                    barcodeDetector.release();
+//                    ToneGenerator toneGenerator = new ToneGenerator(AudioManager.STREAM_NOTIFICATION, 1000);
+//                    toneGenerator.startTone(ToneGenerator.TONE_PROP_BEEP,150);
+//                    String scanResult = qrcodes.valueAt(0).displayValue;
+
+                    txtResult.post(new Runnable() {
+                      @Override
+                      public void run() {
+                          //Create vibrate
+                          Vibrator vibrator = (Vibrator)getApplication().getSystemService(Context.VIBRATOR_SERVICE);
+                          vibrator.vibrate(1000);
+                          txtResult.setText(qrcodes.valueAt(0).displayValue);
+                      }
+                    });
+
+//                    Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+//                    startActivity(intent);
+                }
             }
         });
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        setupScanQRCode();
     }
 }
